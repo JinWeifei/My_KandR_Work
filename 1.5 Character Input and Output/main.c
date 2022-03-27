311a618ee642e9255c8b3699e12964d6ea85c83f
jwf <shenmimowang@126.com>
2022-03-27 20:53:45 +0800

    1.5 all finished1

--- 1.5 Character Input and Output/main.c
+++ 1.5 Character Input and Output/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 main() {
-    int c;
 
-    while ((c = getchar()) != EOF){
+/* 1.5.1 文件复制
+    int c;
+    while ((c = getchar()) != EOF) {
         putchar(c);
+    }
+*/
+
+    long nc;
+
+    nc = 0;
+    while (getchar() != EOF)
+        ++nc;
+    printf("%1d\n", nc);
 
         /*尝试打印EOF
         printf("%f",EOF);
         printf("%d",EOF);
         putchar(EOF);
         */
-    }
 }
