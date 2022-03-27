3179c0aa39890a78ffc9debf4e1becb639edfbbe
jwf <shenmimowang@126.com>
2022-03-28 01:16:05 +0800

    1.5 all finished And I add an outside function

---
+++ 1.5 Character Input and Output/cmake-build-debug/file_copy.c
@@ -0,0 +1,9 @@
+//
+// Created by jwf on 2022/3/27.
+//
+file_copy(){
+    int c;
+    while ((c = getchar()) != 'a') {
+        putchar(c);
+    }
+}
--- 1.5 Character Input and Output/main.c
+++ 1.5 Character Input and Output/main.c
@@ -1,20 +1,54 @@
 #include <stdio.h>
+#include "/Users/jwf/CLionProjects/1.5 Character Input and Output/cmake-build-debug/file_copy.c"
 
-main() {
+#define IN 1
+#define OUT 2
+
+main()
+{
+    /*
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != 'z'){
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT){
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+    */
 
 /* 1.5.1 文件复制
     int c;
-    while ((c = getchar()) != EOF) {
+    while ((c = getchar()) != 'a') {
         putchar(c);
     }
 */
+//尝试引用了外部函数
+    file_copy();
+/* 1.5.2 文件计数
+    double nc;
 
-    long nc;
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
+*/
 
-    nc = 0;
-    while (getchar() != EOF)
-        ++nc;
-    printf("%1d\n", nc);
+/* 1.5.3 行计数
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if (c == "\n")
+            ++nl;
+    printf("%d\n", nl);
+*/
 
         /*尝试打印EOF
         printf("%f",EOF);
