cabcd5493bdb98d85efe41da8d93fdbaf764cafe
jwf <shenmimowang@126.com>
2022-03-29 01:59:19 +0800

    1.10 added

---
+++ 1.10 External Varibles and Scope/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int getlines();
+void copy();
+
+main()
+{
+    int len;
+    extern int max;
+    extern char longest[];
+
+    max = 0;
+    while ((len = getlines()) > 0)
+        if (len > max){
+            max = len;
+            copy();
+        }
+    if (max > 0)
+        printf("%s", longest);
+    return 0;
+}
+
+int getlines()
+{
+    int c,i;
+    extern char line[];
+
+    for (i = 0; i < MAXLINE - 1 && (c = getchar()) != 'a' && c != '\n'; ++i)
+            line[i] = c;
+    if (c == '\n'){
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+    return i;
+}
+
+void copy()
+{
+    int i;
+    extern char line[], longest[];
+
+    i = 0;
+    while ((longest[i] = line[i]) != '\0')
+        ++i;
+}
